85fece074fff1801d2cead7919a72156eac8dd32
skabilesh07 <kabileshs239@gmail.com>
2024-11-06 15:22:00 +0530

    fully completed

--- index.js
+++ index.js
@@ -50,26 +50,49 @@ app.post("/approved", async (req, res) => {
     const formattedDate = today.toISOString().split('T')[0];
     console.log(formattedDate);
 
-    if (!selectedUsers) {
-        console.log("No users selected.");
-        return res.send("No users selected.");
+    if (!selectedUsers && action==="approve page") {
+        console.log("hi");
+        res.redirect("/display2");
+        
     }
-
+    else if (!selectedUsers && action==="approve back") {
+        console.log("hi");
+        res.redirect("/display");
+        
+    }
+    else if (!selectedUsers && action==="publish page") {
+        console.log("hi");
+        res.redirect("/display3");
+        
+    }
+    else if (!selectedUsers && action==="publish back") {
+        console.log("hi");
+        res.redirect("/display2");
+        
+    }
     try {
         let update = {};
         if (action === "approve") {
             update = { IsApproved: true, VersionNumber: versionNo, ApprovedDate: formattedDate };
+
         } else if (action === "publish") {
             update = { IsPublished: true, VersionNumber: versionNo, PublishedDate: formattedDate };
         }
+        
 
         const result = await UpdatedModel.updateMany(
             { _id: { $in: selectedUsers } },
             { $set: update }
         );
-
+        if(action==="approve"){
+            res.redirect("/display2");
+        }
+        if(action==="publish"){
+            res.redirect("/display3");
+        }
+        
         console.log(`${result.modifiedCount} documents updated successfully.`);
-        res.redirect("/display");
+        
     } catch (error) {
         console.error("Error updating users:", error);
         res.status(500).send("Error processing users.");
@@ -80,6 +103,24 @@ app.get("/display", async (req, res) => {
     try {
         const users = await UpdatedModel.find({});
         res.render("open", { users });
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        res.status(500).send("Error fetching data");
+    }
+});
+app.get("/display2", async (req, res) => {
+    try {
+        const users = await UpdatedModel.find({});
+        res.render("display", { users });
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        res.status(500).send("Error fetching data");
+    }
+});
+app.get("/display3", async (req, res) => {
+    try {
+        const users = await UpdatedModel.find({});
+        res.render("final", { users });
     } catch (error) {
         console.error("Error fetching data:", error);
         res.status(500).send("Error fetching data");
